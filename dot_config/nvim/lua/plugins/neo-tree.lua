74d399dc5fc250c15f21391be32de162918ff6a4
kbrdn1 <kylianb1@icloud.com>
2024-03-29 12:41:11 +0100

    🚩 feat: add keymap for Neotree floating window

--- dot_config/nvim/lua/plugins/neo-tree.lua
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -9,7 +9,8 @@ return {
                 -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
         keys = {
-                { "<leader>e", "<cmd>Neotree toggle reveal position=left<cr>", desc = "Toggle Neotree" }
+                { "<leader>e", "<cmd>Neotree toggle reveal position=float<cr>", desc = "Toggle Neotree window" },
+                { "<leader>h", "<cmd>Neotree toggle reveal position=left<cr>",  desc = "Toggle Neotree left pane" }
         },
         opts = {
                 filesystem = {
