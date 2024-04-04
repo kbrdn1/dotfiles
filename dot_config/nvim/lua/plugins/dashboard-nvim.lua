f398d844d7be96c1b8adc4bd904f0e309e3b8918
kbrdn1 <kylianb1@icloud.com>
2024-04-04 02:42:54 +0200

    🚩 feat: change nvim dashboard title

--- dot_config/nvim/lua/plugins/dashboard-nvim.lua
+++ dot_config/nvim/lua/plugins/dashboard-nvim.lua
@@ -1,9 +1,20 @@
+local logo = [[
+    ██╗  ██╗██████╗ ██████╗ ██████╗ ███╗   ██╗
+    ██║ ██╔╝██╔══██╗██╔══██╗██╔══██╗████╗  ██║
+    █████╔╝ ██████╔╝██████╔╝██║  ██║██╔██╗ ██║
+    ██╔═██╗ ██╔══██╗██╔══██╗██║  ██║██║╚██╗██║
+    ██║  ██╗██████╔╝██║  ██║██████╔╝██║ ╚████║
+    ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═══╝
+    ]]
+logo = string.rep("\n", 6) .. logo .. "\n\n"
+
 return {
   "nvimdev/dashboard-nvim",
   event = "VimEnter",
   config = function()
     require("dashboard").setup({
       config = {
+        header = vim.split(logo, "\n"),
         project = {
           enable = true,
           limit = 4,
